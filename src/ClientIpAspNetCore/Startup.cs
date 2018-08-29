506f10b1bd7b9d1f6b143529bb13e3a6fe94498b
damienbod <damien_bod@hotmail.com>
2018-08-29 22:29:13 +0200

    Updating to .NET Core 2.1

--- src/ClientIpAspNetCore/Startup.cs
+++ src/ClientIpAspNetCore/Startup.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using ClientIpAspNetCore.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -30,7 +31,7 @@ namespace ClientIpAspNetCore
         {
             services.AddScoped<ClientIdCheckFilter>();
 
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
         
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
